84fde82ac5ac6008d2e4d941698c9f4ab14693a4
andreasalvi99 <andrea.salvi.r@gmail.comm>
2025-12-18 20:31:48 +0100

    2nd bonus task done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -6,10 +6,19 @@ for (let i = teachers.length - 1; i >= 0; i--) {
 }
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = null;
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    console.log(teachers[i]);
+    longNames.push(teachers[i]);
+  }
+}
+
+console.log(longNames);
+
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(1, 1);
 console.table(teachers);
@@ -26,12 +35,10 @@ for (let i = 0; i < teachers.length; i++) {
     isFabioIn = true;
   }
 }
-
 console.log("Insegnante trovato?", isFabioIn);
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = isFabioIn;
 console.log(isFabioPresent);
-
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join(", ");
 console.log(teachersString);
